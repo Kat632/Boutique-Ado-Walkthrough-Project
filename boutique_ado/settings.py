35995b7094bf2fb1688d843ab53b1d1033640c5a
Kat632 <87238917+Kat632@users.noreply.github.com>
2022-10-30 18:35:46 +0000

    Update settings.py

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['kb-boutique-ado.herokuapp.com', 'localhost', kb-boutique-ado.up.railway.app/]
+ALLOWED_HOSTS = ['kb-boutique-ado.herokuapp.com', 'localhost', 'kb-boutique-ado.up.railway.app']
 
 # Application definition
 
